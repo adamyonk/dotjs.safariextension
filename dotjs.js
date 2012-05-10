8c326366f790a01441aa5f8c0a7194925344befd
Adam Jahnke <adamyonk@gmail.com>
2012-05-10 17:48:00 -0500

    Adding check for CSS files

--- dotjs.js
+++ dotjs.js
@@ -8,3 +8,14 @@ $.ajax({
     console.log('no dotjs server found at localhost:3131')
   }
 })
+
+$.ajax({
+  url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.css',
+  dataType: 'text',
+  success: function(d){
+    $('head').append($('<style>').text(d));
+  },
+  error: function(){
+    console.log('no dotjs server found at localhost:3131')
+  }
+})
